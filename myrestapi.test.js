663301903977d87cba635cf2319442dbbbc8a5a4
Veerawut <veerawut28122545@gmail.com>
2026-03-25 11:40:41 +0700

    add 2 more test cases

--- myrestapi.test.js
+++ myrestapi.test.js
@@ -4,4 +4,10 @@ describe('test hr zone calculation', () => {
   test('test - zone number is correct?', () => {
     expect(restapi.calculateHR(30,180).zone).toBe(5);
   });
+  test('test - hr ขอบเขตล่างถูกต้องหรือไม่', () => {
+  expect (restapi.calculateHR (30,180) .lb) .toBe (171);
+});
+test('test - hr ขอบเขตบนถูกต้องหรือไม่', () => {
+expect (restapi.calculateHR (30,180) .ub) .toBe (190);
+});
 });
